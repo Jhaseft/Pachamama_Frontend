38ad77669410d61ad819cf4c6e2f4c17bd4e03e5
Jhaseft <jhasesaat@gmail.com>
2026-03-28 18:44:34 -0400

    cambios back

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // src/config.ts
-export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? "https://caja-negra-pachamama-backend.wkhbmg.easypanel.host";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? "https://caja-negra-pacha-back.wkhbmg.easypanel.host";
 export const AGORA_APP_ID = process.env.EXPO_PUBLIC_AGORA_APP_ID ?? "0f5a7945c4de4aeaa704902064e1f8f9";
